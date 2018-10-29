20ed96727510bc7221adf027666f7d2e83d7543e
Lipe Teixeira <teixeira.lipe@gmail.com>
2018-10-28 22:55:04 -0400

    added MVC + CRUD + DB + CSS + regex

---
+++ assignment02/assignment02/Data/assignment02Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace assignment02.Models
+{
+    public class assignment02Context : DbContext
+    {
+        public assignment02Context (DbContextOptions<assignment02Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<assignment02.Models.Member> Member { get; set; }
+    }
+}
