d6ab027cde0c6fd7b440887c863d349ef320b017
Lipe Teixeira <teixeira.lipe@gmail.com>
2018-11-16 15:26:23 -0500

    added LinkedInAuthentication assignment

---
+++ LinkedInAuthentication/LinkedInAuthentication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LinkedInAuthentication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
